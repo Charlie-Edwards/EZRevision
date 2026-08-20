ef4249ae2fa932ff08691c883e2067cbdade4375
Charlie Edwards <285464439+Charlie-Edwards@users.noreply.github.com>
2026-08-20 21:24:20 +0200

    Bot layout

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.getenv("TOKEN"))
